f621d0c7b19b492289a91912077b5d06fb7b0373
Administrator <Administrator@7A304-0001>
2016-12-07 11:47:15 +0800

    ListView基本演示

--- src/com/example/helloworld/fragments/pages/FeedListFragment.java
+++ src/com/example/helloworld/fragments/pages/FeedListFragment.java
@@ -1,23 +1,70 @@
 package com.example.helloworld.fragments.pages;
 
 import com.example.helloworld.R;
-
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class FeedListFragment extends Fragment {
 
 	View view;
+	ListView listView;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		if (view==null){
 			view = inflater.inflate(R.layout.fragment_page_feed_list, null);
+			
+			listView = (ListView) view.findViewById(R.id.list);
+			listView.setAdapter(listAdapter);
 		}
 
 		return view;
 	}
+	
+	BaseAdapter listAdapter = new BaseAdapter() {
+		
+		@SuppressLint("InflateParams")
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = null;
+			
+			if(convertView==null){
+				LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+				view = inflater.inflate(android.R.layout.simple_list_item_1, null);	
+			}else{
+				view = convertView;
+			}
+			
+			TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+			text1.setText("THIS IS ROW "+position);
+			
+			return view;
+		}
+		
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+		
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return "";
+		}
+		
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 20;
+		}
+	};
 }
